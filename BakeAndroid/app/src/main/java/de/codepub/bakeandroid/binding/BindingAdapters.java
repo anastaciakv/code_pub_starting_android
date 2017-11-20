2683174a054bb4be54b7e1212a517749d1c1e6f2
Anastasiia Korostelova <anastaciakv@gmail.com>
2017-11-20 15:58:51 +0100

    Step 3 - data binding

---
+++ BakeAndroid/app/src/main/java/de/codepub/bakeandroid/binding/BindingAdapters.java
@@ -0,0 +1,26 @@
+package de.codepub.bakeandroid.binding;
+
+
+import android.databinding.BindingAdapter;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+//todo read more: https://developer.android.com/topic/libraries/data-binding/index.html
+
+public class BindingAdapters {
+
+    @BindingAdapter("visibleGone")
+    public static void showHide(View view, boolean show) {
+        view.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("imageUrl")
+    public static void bindImage(ImageView imageView, String url) {
+        if (url == null || url.isEmpty()) return;
+        Picasso.with(imageView.getContext())
+                .load(url)
+                .error(android.R.drawable.stat_notify_error)
+                .into(imageView);
+    }
+}
