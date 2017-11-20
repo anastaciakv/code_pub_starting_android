384abc5f37f4902e82e668bc055df07d56aeb1f4
Anastasiia Korostelova <anastaciakv@gmail.com>
2017-11-20 16:38:12 +0100

    Step 5 - added Client API

---
+++ BakeAndroid/app/src/main/java/de/codepub/bakeandroid/data/ApiClient.java
@@ -0,0 +1,9 @@
+package de.codepub.bakeandroid.data;
+
+
+public interface ApiClient {
+    //todo BASE URL
+    String BASE_URL = "https://myserver.net/";
+
+    //todo API methods
+}
--- BakeAndroid/app/src/main/java/de/codepub/bakeandroid/di/AppModule.java
+++ BakeAndroid/app/src/main/java/de/codepub/bakeandroid/di/AppModule.java
@@ -1,8 +1,47 @@
 package de.codepub.bakeandroid.di;
 
+import com.google.gson.Gson;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import de.codepub.bakeandroid.BuildConfig;
+import de.codepub.bakeandroid.data.ApiClient;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module()
 public class AppModule {
     //todo declare all object instances here
+
+    @Singleton
+    @Provides
+    ApiClient provideApiClient() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .readTimeout(1, TimeUnit.MINUTES)
+                .connectTimeout(1, TimeUnit.MINUTES);
+
+        if (BuildConfig.DEBUG)
+            enableLogging(httpClient);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ApiClient.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .client(httpClient.build())
+                .build();
+
+        return retrofit.create(ApiClient.class);
+    }
+
+    private void enableLogging(OkHttpClient.Builder httpClient) {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClient.addInterceptor(logging);
+    }
 }
